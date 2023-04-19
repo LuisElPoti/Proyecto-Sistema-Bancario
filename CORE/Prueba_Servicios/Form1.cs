1615b33cd143bdc4c977688407410853af750724
JESUS ANTONIO ACOSTA CABRERA <1104174@est.intec.edu.do>
2023-04-18 22:17:40 -0400

    Prueba de Servicios

---
+++ CORE/Prueba_Servicios/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_Servicios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clientes.WSClientesClient Cliente = new Clientes.WSClientesClient();
+
+            Cliente.AgregarCliente("Miguel",1,"402566","Miguel@gmail.com","8296697544","Avenida",DateTime.Now);
+
+            MessageBox.Show("Servicio Usado");
+        }
+    }
+}
