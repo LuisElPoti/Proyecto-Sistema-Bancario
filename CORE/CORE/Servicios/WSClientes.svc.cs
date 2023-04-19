3b802ff8ac077d45531e9c48299daf9d39afafe3
Luis Adames <adames1601@gmail.com>
2023-04-19 00:01:40 -0400

    cambios clientes

--- CORE/CORE/Servicios/WSClientes.svc.cs
+++ CORE/CORE/Servicios/WSClientes.svc.cs
@@ -7,7 +7,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace CORE
+namespace CORE.Servicios
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Servicios" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Servicios.svc or Servicios.svc.cs at the Solution Explorer and start debugging.
