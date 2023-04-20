e447e5e304b9869e0f7c9f8f7053b0904cf06398
JESUS ANTONIO ACOSTA CABRERA <1104174@est.intec.edu.do>
2023-04-20 01:16:08 -0400

    Intento WebService

---
+++ CORE/CoreServices/OperacionesCuenta.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+
+namespace CoreServices.Clases
+{
+    public class OperacionesCuenta
+    {        
+        public spGetAllCuenta_Result[] GetCuentas()
+        {
+            using (DBCoreEntities db = new DBCoreEntities())
+            {
+                return db.spGetAllCuenta().ToArray();
+            }
+        }
+
+
+        /*
+         * public List<CollegeDetail> GetCollegeRecords()  
+        {  
+            using (CollegeDataEntities context = new CollegeDataEntities())  
+            {  
+                return context.CollegeDetails.ToList();  
+            }  
+        }
+        */
+
+        public bool InsertCuentas(int idCliente, int idTipoCuenta, int idBanco, string NumeroCuenta, bool Estado)
+        {
+            using (DBCoreEntities db = new DBCoreEntities())
+            {
+                ObjectParameter ReturnedValue = new ObjectParameter("ReturnValue", typeof(int));
+                db.spInsertCuenta(idCliente, idTipoCuenta, idBanco, NumeroCuenta, Estado);
+
+                if (Convert.ToInt32(ReturnedValue.Value) >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }            
+        }
+
+        public bool UpdateCuentas(int idCuenta, bool Estado, decimal Balance)
+        {
+            using (DBCoreEntities db = new DBCoreEntities())
+            {
+                int ReturnedValue = db.spUpsertCuenta(idCuenta, Estado, Balance);
+
+                if (ReturnedValue >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool DeleteCuentas(int id)
+        {
+            using (DBCoreEntities db = new DBCoreEntities())
+            {
+                int ReturnedValue = db.spDeleteCuenta(id);
+
+                if (ReturnedValue >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public Cuenta GetCuentabyID(int id)
+        {
+            using (DBCoreEntities db = new DBCoreEntities())
+            {
+                Cuenta Cuenta = null;
+
+                try
+                {
+                    Cuenta Correspondiente = db.Cuenta.First((p) => p.idCuenta == id);
+
+                    Cuenta = new Cuenta();
+                    Cuenta.idCuenta = Correspondiente.idCuenta;
+                    Cuenta.idTipoCuenta = Correspondiente.idTipoCuenta;
+                    Cuenta.idCliente = Correspondiente.idCliente;
+                    Cuenta.idBanco = Correspondiente.idBanco;
+                    Cuenta.NumeroCuenta = Correspondiente.NumeroCuenta;
+                    Cuenta.Balance = Correspondiente.Balance;
+                    Cuenta.Estado = Correspondiente.Estado;
+                    Cuenta.FechaIngreso = Correspondiente.FechaIngreso;
+
+                    return Cuenta;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
