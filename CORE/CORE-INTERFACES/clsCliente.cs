65809bf4232cb9454da33fa04443bd87f72a67ef
Luis Adames <adames1601@gmail.com>
2023-04-21 07:17:20 -0400

    no tengo fuerza

---
+++ CORE/CORE-INTERFACES/clsCliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE_INTERFACES
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string TipoDocumento { get; set; }
+        public string Documento { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
