b04c2898806830ec1b4af729b7423ceefa4814b5
HezzTia <emilycabreraarache@gmail.com>
2023-04-21 01:40:08 -0400

    mas pantallas de caja

--- CAJA/CAJA/cajaC.cs
+++ CAJA/CAJA/cajaC.cs
@@ -16,5 +16,53 @@ namespace CAJA
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txt_usuario_login_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cajaC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            menu m1 = new menu();
+            this.Hide();
+            m1.ShowDialog();
+            this.Close();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
