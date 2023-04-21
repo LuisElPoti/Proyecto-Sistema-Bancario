9b9d79220362e65d0ce16473c943cf8b743d6d95
GleidyEspinal <1104225@est.intec.edu.do>
2023-04-21 09:01:41 -0400

    ajustes bancking 2.

--- NETBANKING/NETBANKING/LoginPages/LoginForm.aspx.cs
+++ NETBANKING/NETBANKING/LoginPages/LoginForm.aspx.cs
@@ -25,30 +25,7 @@ namespace NETBANKING
 
         protected void btnAceptar_Click(object sender, EventArgs e)
         {
-            string connectionString = "Data Source=sistema-bancario-server.database.windows.net;Initial Catalog=DBNetBanking;Persist Security Info=True;User ID=Administrador;Password=***********;MultipleActiveResultSets=True;Application Name=EntityFramework";
-            SqlConnection connection = new SqlConnection(connectionString);
-
-            connection.Open();
-
-            string usuario = txtUsuario.Text;
-            string clave = txtClave.Text;
-
-            string sql = "SELECT dbo.fnValidarUsuario(@Usuario, @Usuario)";
-            SqlCommand cmd = new SqlCommand(sql, connection);
-            cmd.Parameters.AddWithValue("@Usuario", txtUsuario.Text);
-            cmd.Parameters.AddWithValue("@Clave", txtClave.Text);
-            int resultado = (int)cmd.ExecuteScalar();
-
-            if (resultado == 1)
-            {
-                Response.Redirect("InicioForm.aspx");
-            }
-            else
-            {
-                Response.Redirect("InicioForm.aspx");
-
-
-            }
+            
 
 
         }
