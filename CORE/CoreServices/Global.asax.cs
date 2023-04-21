ef2e4f364974429998a2b66744f7d89a3feef769
Luis Adames <adames1601@gmail.com>
2023-04-20 23:51:29 -0400

    prueba restablecer

--- CORE/CoreServices/Global.asax.cs
+++ CORE/CoreServices/Global.asax.cs
@@ -19,6 +19,7 @@ namespace CoreServices
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            XmlConfigurator.Configure();
         }
     }
 }
